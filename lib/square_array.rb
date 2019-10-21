54783972454740db5cf6a2af274d688a3cca4888
Craig Storer <craigstorer@gmail.com>
2019-10-21 17:46:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(numbers)
   counter = 0
   while numbers[counter] do
-     puts numbers[counter]
+     puts numbers[counter] * numbers[counter]
+  # numbers = [1,2,3]
   counter += 1
   end
 end
